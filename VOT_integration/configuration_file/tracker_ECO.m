2d59e96996b7cb15ff7cc3aa3ee7ff1c39894ed3
rockkingjy <rockking.jy@gmail.com>
2018-05-26 12:45:50 +0900

    update

--- VOT_integration/configuration_file/tracker_ECO.m
+++ VOT_integration/configuration_file/tracker_ECO.m
@@ -4,5 +4,6 @@
 ECO_repo_path = ########
 
 tracker_label = 'ECO';
-tracker_command = generate_matlab_command('benchmark_tracker_wrapper(''ECO'', ''VOT2016_DEEP_settings'', true)', {[ECO_repo_path '/VOT_integration/benchmark_wrapper']});
+tracker_command = generate_matlab_command('benchmark_tracker_wrapper(''ECO'', ''VOT2016_DEEP_settings'', true)', ...
+                                            {[ECO_repo_path '/VOT_integration/benchmark_wrapper']});
 tracker_interpreter = 'matlab';
