e50293a967d70b58cc1b6c708a5d8fb335c0c77e
rockkingjy <rockking.jy@gmail.com>
2018-07-24 12:50:31 +0900

    :+1:

--- implementation/training/lhs_operation.m
+++ implementation/training/lhs_operation.m
@@ -23,16 +23,18 @@ t1 = toc();
 % (blockwise matrix multiplications)
 %implements: A' diag(sample_weights) A f
 
-% 2: sum over all features and feature blocks
+% 2: sum over all features and feature blocks-------------------------------
+% samplesf(k1}: 30 x 10 x 25 x 13
+% permute: 25 x 13 x 10 -> 10 x 1 x 25 x 13
+% sh: 30 x 1 x 25 x 13
 sh = mtimesx(samplesf{k1}, permute(hf{k1}, [3 4 1 2]), 'speed');    % assumes the feature with the highest resolution is first
 pad_sz = cell(1,1,num_features);
-for k = block_inds
-    pad_sz{k} = (output_sz - [size(hf{k},1), 2*size(hf{k},2)-1]) / 2;
-    
-    sh(:,1,1+pad_sz{k}(1):end-pad_sz{k}(1), 1+pad_sz{k}(2):end) = ...
-        sh(:,1,1+pad_sz{k}(1):end-pad_sz{k}(1), 1+pad_sz{k}(2):end) + mtimesx(samplesf{k}, permute(hf{k}, [3 4 1 2]), 'speed');
+for k = block_inds % num_features
+    pad_sz{k} = (output_sz - [size(hf{k}, 1), 2 * size(hf{k}, 2) - 1]) / 2;
+    sh(:, 1, 1 + pad_sz{k}(1): end - pad_sz{k}(1), 1 + pad_sz{k}(2): end) = ...
+        sh(:, 1, 1 + pad_sz{k}(1): end - pad_sz{k}(1), 1 + pad_sz{k}(2): end) ...
+        + mtimesx(samplesf{k}, permute(hf{k}, [3 4 1 2]), 'speed');
 end
-
 % weight all the samples
 sh = bsxfun(@times,sample_weights,sh);
 
@@ -40,11 +42,13 @@ t2 = toc();
 disp(['update train time3_1_2 ' num2str(t2-t1)]);
 t1 = toc();
 
-% 3: multiply with the transpose
+% 3: multiply with the transpose----------------------------------------------
 hf_out = cell(1,1,num_features);
 hf_out{k1} = permute(conj(mtimesx(sh, 'C', samplesf{k1}, 'speed')), [3 4 2 1]);
-for k = block_inds
-    hf_out{k} = permute(conj(mtimesx(sh(:,1,1+pad_sz{k}(1):end-pad_sz{k}(1), 1+pad_sz{k}(2):end), 'C', samplesf{k}, 'speed')), [3 4 2 1]);
+for k = block_inds % num_features
+    hf_out{k} = permute(conj(mtimesx(...
+        sh(:, 1, 1 + pad_sz{k}(1): end - pad_sz{k}(1),1 + pad_sz{k}(2):end), ...
+                                     'C', samplesf{k}, 'speed')), [3 4 2 1]);
 end
 
 t2 = toc();
@@ -53,7 +57,7 @@ t1 = toc();
 
 % 4: compute the operation corresponding to the regularization term (convolve
 % each feature dimension with the DFT of w, and the tramsposed operation)
-% add the regularization part
+% add the regularization part-------------------------------------------------
 % hf_conv = cell(1,1,num_features);
 for k = 1:num_features
     reg_pad = min(size(reg_filter{k},2)-1, size(hf{k},2)-1);
@@ -67,7 +71,7 @@ for k = 1:num_features
     % do final convolution and put toghether result
     hf_out{k} = hf_out{k} + convn(hf_conv(:,1:end-reg_pad,:), reg_filter{k}, 'valid');
 end
-
+% hf_out: 25 x 13 x 10
 
 t2 = toc();
 disp(['update train time3_1_4 ' num2str(t2-t1)]);
